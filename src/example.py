f3c159c8769c12326ce8153a2bf6997696b81ace
Kai Eckert <hallo@kaiec.de>
2021-05-19 00:20:46 +0200

    Refactored API, many comments and key presses

--- src/example.py
+++ src/example.py
@@ -3,15 +3,22 @@ from pystage import Sprite, Stage
 stage = Stage(500, 500)
 sprite = stage.create_sprite()
 
-def do_something():
-    sprite.x = 250
-    sprite.y = 250
+def do_something(self):
+    self.x = 250
+    self.y = 250
     yield 0
     for i in range(4):
-        sprite.move(20)
-        sprite.turn_left(90)
+        self.move(20)
+        self.turn_left(90)
         yield 1
 
-sprite.do(do_something)
+sprite.when_program_is_started(do_something)
+
+
+def react_to_key(self):
+    self.move(10)
+    yield
+
+sprite.when_key_is_pressed(" ", react_to_key)
 
 stage.play()
--- src/pystage/__init__.py
+++ src/pystage/__init__.py
@@ -1,29 +1,125 @@
 import pygame
 import math
+import inspect
+import pkg_resources
+import collections
+
 
 def _deg2rad(deg):
     return deg / 360 * 2 * math.pi
+
     
 def _rad2deg(rad):
     return rad / (2 * math.pi) * 360
+
+
+key_mappings = {
+        "a": pygame.K_a,
+        "b": pygame.K_b,
+        "c": pygame.K_c,
+        "d": pygame.K_d,
+        "e": pygame.K_e,
+        "f": pygame.K_f,
+        "g": pygame.K_g,
+        "h": pygame.K_h,
+        "i": pygame.K_i,
+        "j": pygame.K_j,
+        "k": pygame.K_k,
+        "l": pygame.K_l,
+        "m": pygame.K_m,
+        "n": pygame.K_n,
+        "o": pygame.K_o,
+        "p": pygame.K_p,
+        "q": pygame.K_q,
+        "r": pygame.K_r,
+        "s": pygame.K_s,
+        "t": pygame.K_t,
+        "u": pygame.K_u,
+        "v": pygame.K_v,
+        "w": pygame.K_w,
+        "x": pygame.K_x,
+        "y": pygame.K_y,
+        "z": pygame.K_z,
+        "1": pygame.K_1,
+        "2": pygame.K_2,
+        "3": pygame.K_3,
+        "4": pygame.K_4,
+        "5": pygame.K_5,
+        "6": pygame.K_6,
+        "7": pygame.K_7,
+        "8": pygame.K_8,
+        "9": pygame.K_9,
+        "0": pygame.K_0,
+        "left": pygame.K_LEFT,
+        "right": pygame.K_RIGHT,
+        "up": pygame.K_UP,
+        "down": pygame.K_DOWN,
+        "space": pygame.K_SPACE,
+        " ": pygame.K_SPACE,
+        }
+
 
 
 class CodeBlock():
     '''
     The CodeBlock encapsulates a generator and manages its state.
     '''
+    last_id = -1
 
-    def __init__(self, sprite, generator):
-        self.sprite = sprite
-        self.name = generator.__name__
+    def __init__(self, sprite_or_stage, generator, name=""):
+        if len(inspect.signature(generator).parameters)!=1:
+            raise ValueError(f"Your code block '{generator.__name__}' needs one parameter, usually called self.")
+
+        self.sprite_or_stage = sprite_or_stage
+        if name!="" and name!=None:
+            name = f"-{name}"
+        CodeBlock.last_id += 1
+        # The name of a code block is unique with a global id.
+        # A custom name may be provided to distinguish blocks
+        # that use the same generator.
+        # This is not possible in Scratch (you only can copy blocks), 
+        # but could be allowed here.
+        # TODO: Discuss, if we should completely prohibit the 
+        # reuse of generators, it could make things easier.
+        self.name = f"{generator.__name__}{name}-{CodeBlock.last_id}"
+        # A copy of the generator used for restarts (e.g. when bound to events)
         self.generator = generator
-        self.steps = generator()
+        # The current state of a generator after it is started
+        self.steps = generator(sprite_or_stage)
+        if not isinstance(self.steps, collections.Iterable):
+            raise ValueError(f"Your code block '{generator.__name__}' needs at least one yield statement.")
+        # The time until the next step is executed
         self.wait_time = 0
-        self.stopped = False
+        # Flag indicating if the block is currently running
+        self.running = False
+
+
+    def start_if_not_running(self):
+        '''
+        Start the code block. If the block is already started,
+        this method does nothing.
+        '''
+        if self.running:
+            return
+        self.start_or_restart()
+
+
+    def start_or_restart(self):
+        '''
+        (Re-)start the code block. If the block is already started,
+        the current execution will not continue.
+        '''
+        self.running = True
+        self.wait_time = 0
+        self.steps = self.generator(self.sprite_or_stage)
+        print(f"Start of {self.name} triggered.")
 
 
     def update(self, dt):
-        if self.stopped:
+        '''
+        Check if it is time for the next step and execute it.
+        '''
+        if not self.running:
             return
         self.wait_time -= dt
         if self.wait_time < 0:
@@ -34,110 +130,107 @@ class CodeBlock():
                     self.wait_time = result
             except StopIteration:
                 print(f"CodeBlock {self.name} has finished.")
-                self.stopped = True
-
-
+                self.running = False
 
 
 class Sprite():
-    image = pygame.image.load("images/zombie_idle.png")
+    image = pygame.image.load(pkg_resources.resource_filename(__name__, "images/zombie_idle.png"))
     x = 0.0
     y = 0.0
     direction = 0
     pen = False
     color = (255,0,0)
+    # name: code_block
     code_blocks = {}
+    # pygame.K_?: [name, ...]
+    key_pressed_blocks = {}
 
 
     def __init__(self, stage):
         self.stage = stage
 
-    def update(self, dt):
+
+    def _draw(self):
+        self.stage.screen.blit(self.image, (self.x, self.y))
+
+
+    def _update(self, dt):
         for name in self.code_blocks:
             self.code_blocks[name].update(dt)
 
-    def do(self, generator):
-        self.code_blocks[generator.__name__] = CodeBlock(self, generator)
 
-    def when_key_pressed(self, key, generator):
-        pass
+    def _process_key_pressed(self, key):
+        # key is a pygame constant, e.g. pygame.K_a
+        # This hat block is special as it only fires again when the code block has ended. 
+        # All other hat block methods stop the current execution and restart the block.
+        if key in self.key_pressed_blocks:
+            for name in self.key_pressed_blocks[key]:
+                self.code_blocks[name].start_if_not_running()
 
 
-    def draw(self):
-        self.stage.screen.blit(self.image, (self.x, self.y))
+    def _register_code_block(self, generator, name=""):
+        new_block = CodeBlock(self, generator, name)
+        self.code_blocks[new_block.name] = new_block
+        print(f"New code block registered: {new_block.name}")
+        return new_block
+
+
+    def when_program_is_started(self, generator, name=""):
+        new_block = self._register_code_block(generator, name)
+        new_block.start_if_not_running()
+        print(f"Bound to start: {new_block.name}")
+
+
+    def when_key_is_pressed(self, key, generator, name=""):
+        '''
+        Adds the code block to the event queue for key presses.
+        '''
+        new_block = self._register_code_block(generator, name)
+        if key not in key_mappings:
+            # TODO: implement "any" key.
+            raise ValueError(f"Bad key: {key}. Only a-z, 0-9 and space are allowed.")
+        pg_key = key_mappings[key]
+        # No defaultdict so that we can easily check if a key mapping is available
+        if pg_key not in self.key_pressed_blocks:
+            self.key_pressed_blocks[pg_key] = []
+        self.key_pressed_blocks[pg_key].append(new_block.name)
+        print(f"Bound to key press ({key}/{pg_key}): {new_block.name}")
+
 
     def pen_down(self):
         self.pen = True
 
+
     def pen_up(self):
         self.pen = False
-    
+
+
     def pen_toggle(self):
         self.pen = not self.pen
 
+
     def set_pen_color_to(self, color):
         self.color = color
+
 
     def go_to_x_y(self, x, y):
         self.x = x
         self.y = y
 
+
     def turn_left(self, deg):
         self.direction -= deg
 
+
     def turn_right(self, deg):
         self.direction += deg
+
 
     def move(self, steps):
         old_x = self.x
         old_y = self.y
         self.x = self.x + steps * math.cos(_deg2rad(self.direction))
         self.y = self.y + steps * math.sin(_deg2rad(self.direction))
-
-
-key_mappings = {
-    pygame.K_a: ["a"],
-    pygame.K_b: ["b"],
-    pygame.K_c: ["c"],
-    pygame.K_d: ["d"],
-    pygame.K_e: ["e"],
-    pygame.K_f: ["f"],
-    pygame.K_g: ["g"],
-    pygame.K_h: ["h"],
-    pygame.K_i: ["i"],
-    pygame.K_j: ["j"],
-    pygame.K_k: ["k"],
-    pygame.K_l: ["l"],
-    pygame.K_m: ["m"],
-    pygame.K_n: ["n"],
-    pygame.K_o: ["o"],
-    pygame.K_p: ["p"],
-    pygame.K_q: ["q"],
-    pygame.K_r: ["r"],
-    pygame.K_s: ["s"],
-    pygame.K_t: ["t"],
-    pygame.K_u: ["u"],
-    pygame.K_v: ["v"],
-    pygame.K_w: ["w"],
-    pygame.K_x: ["x"],
-    pygame.K_y: ["y"],
-    pygame.K_z: ["z"],
-    pygame.K_1: ["1"],
-    pygame.K_2: ["2"],
-    pygame.K_3: ["3"],
-    pygame.K_4: ["4"],
-    pygame.K_5: ["5"],
-    pygame.K_6: ["6"],
-    pygame.K_7: ["7"],
-    pygame.K_8: ["8"],
-    pygame.K_9: ["9"],
-    pygame.K_0: ["0"],
-    pygame.K_LEFT: ["left"],
-    pygame.K_RIGHT: ["right"],
-    pygame.K_UP: ["up"],
-    pygame.K_DOWN: ["down"],
-    pygame.K_SPACE: ["space", " "],
-        }
 
 
 class Stage():
@@ -172,13 +265,14 @@ class Stage():
                 if event.type == pygame.QUIT:
                     self.running = False
                 if event.type == pygame.KEYDOWN:
-                    pass
+                    for sprite in self.sprites:
+                        sprite._process_key_pressed(event.key)
 
             self.screen.fill(self.background_color)
 
             for sprite in self.sprites:
-                sprite.update(dt)
-                sprite.draw()
+                sprite._update(dt)
+                sprite._draw()
 
             pygame.display.flip()
             dt = self.clock.tick(self.FPS) / 1000
