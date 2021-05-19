dbb02ee2eef1632bdda8a0f9a5af3207cd16ce67
Kai Eckert <hallo@kaiec.de>
2021-05-19 23:20:39 +0200

    Control API defined

--- src/pystage/__init__.py
+++ src/pystage/__init__.py
@@ -456,6 +456,44 @@ class Sprite():
     def get_volume(self):
         pass
 
+    ##
+    # Control
+    #
+
+    # Controls obviously should mostly be done in Python
+    # or this would be no help in learning python.
+    
+    # wait is simply yield
+    # repeat is for i in range()
+    # forever is while True
+    # if is if
+    # wait until is while COND: yield
+    # repeat until is while not
+
+    def stop_all(self):
+        # not only in this sprite!
+        pass
+
+    def stop_this_script(self):
+        # This is equivalent to return
+        pass
+
+    def stop_other_scripts_in_sprite(self):
+        pass
+
+    # Cloning is probably tricky.  
+
+    def when_i_start_as_clone(self, generator, name=""):
+        pass
+
+    def create_clone_of_myself(self):
+        pass
+
+    def create_clone_of_sprite(self, sprite):
+        pass
+
+    def delete_this_clone(self):
+        pass
 
     ##
     # Pen Extension
@@ -492,6 +530,9 @@ class Sprite():
 
     def set_pen_size_to(self, value):
         pass
+
+
+
 
 class Stage():
     running = False
