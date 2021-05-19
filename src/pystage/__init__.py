7d6f1da42da9f72072425a894586ae2002180947
Kai Eckert <hallo@kaiec.de>
2021-05-19 22:42:01 +0200

    Looks API defined

--- src/pystage/__init__.py
+++ src/pystage/__init__.py
@@ -218,6 +218,9 @@ class Sprite():
     def set_pen_color_to(self, color):
         self.color = color
 
+    ##
+    # Motion
+    #
 
     def turn_left(self, deg):
         self.direction -= deg
@@ -290,7 +293,6 @@ class Sprite():
         # See Enum RotationStyle above
         pass
 
-
     # Setters and getters are questionable, but 
     # this way we would clearly adapt the Scratch API
     # We could get rid of them for a direct access 
@@ -305,6 +307,98 @@ class Sprite():
     def get_direction(self):
         pass
 
+    ##
+    # Looks
+    #
+    def say_for_time(self, text, secs):
+        pass
+
+
+    def say(self, text):
+        pass
+
+
+    def think_for_time(self, text, secs):
+        pass
+
+    def think(self, text):
+        pass
+
+    def switch_costume_to(self, costume):
+        # TODO: Data/class structure for costumes
+        pass
+
+    def next_costume(self):
+        pass
+
+    def switch_backdrop_to(self, backdrop):
+        # Backdrops are for the stage.
+        # In Scratch, a sprite can change the backdrop.
+        pass
+
+    def next_backdrop(self):
+        pass
+
+    def change_size_by(self, percent):
+        # this is percentage
+        pass
+
+    def set_size_to_percent(self, percent):
+        pass
+
+    def change_effect_by(self, effect, value):
+        # This is tricky, could be implemented in costumes.
+        # Effects: 
+        # - color (this is hue shifting)
+        # - fisheye
+        # - whirl
+        # - pixelate
+        # - mosaic
+        # - brightness
+        # - ghost (this is transparency)
+        pass
+
+    def set_effect_to(self, effect, value):
+        pass
+
+    def clear_graphic_effects(self):
+        pass
+
+    def show(self):
+        pass
+
+    def hide(self):
+        pass
+
+    def go_to_front_layer(self, layer):
+        pass
+
+    def go_to_back_layer(self, layer):
+        pass
+
+    def go_forward_layers(self, value):
+        pass
+
+    def go_backward_layers(self, value):
+        pass
+
+    def get_costume_number(self):
+        # 1-based
+        pass
+
+    def get_backdrop_number(self):
+        pass
+
+
+    def get_costume_name(self):
+        pass
+
+    def get_backdrop_name(self):
+        pass
+
+    def get_size(self):
+        # percent
+        pass
 
 class Stage():
     running = False
