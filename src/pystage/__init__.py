a2a57dcd187a60e454ef05498e21e81a6f9a5175
Kai Eckert <hallo@kaiec.de>
2021-05-19 14:53:55 +0200

    Full Motion API defined (not yet implemented)

--- src/pystage/__init__.py
+++ src/pystage/__init__.py
@@ -3,7 +3,7 @@ import math
 import inspect
 import pkg_resources
 import collections
-
+import enum
 
 def _deg2rad(deg):
     return deg / 360 * 2 * math.pi
@@ -12,6 +12,12 @@ def _deg2rad(deg):
 def _rad2deg(rad):
     return rad / (2 * math.pi) * 360
 
+
+class RotationStyle(enum.Enum):
+    # https://en.scratch-wiki.info/wiki/Rotation_Style
+    ALL_AROUND = 1
+    LEFT_RIGHT = 2
+    DONT_ROTATE = 3
 
 key_mappings = {
         "a": pygame.K_a,
@@ -177,8 +183,8 @@ class Sprite():
 
     def when_program_is_started(self, generator, name=""):
         new_block = self._register_code_block(generator, name)
-        new_block.start_if_not_running()
         print(f"Bound to start: {new_block.name}")
+        new_block.start_or_restart()
 
 
     def when_key_is_pressed(self, key, generator, name=""):
@@ -213,11 +219,6 @@ class Sprite():
         self.color = color
 
 
-    def go_to_x_y(self, x, y):
-        self.x = x
-        self.y = y
-
-
     def turn_left(self, deg):
         self.direction -= deg
 
@@ -231,6 +232,78 @@ class Sprite():
         old_y = self.y
         self.x = self.x + steps * math.cos(_deg2rad(self.direction))
         self.y = self.y + steps * math.sin(_deg2rad(self.direction))
+
+    def go_to_random_position(self):
+        pass
+
+    def go_to_mouse_pointer(self):
+        pass
+
+    def go_to_sprite(self, sprite):
+        pass
+
+    def go_to_x_y(self, x, y):
+        self.x = x
+        self.y = y
+
+    def glide_to_random_position(self, secs):
+        # This requires special care, either this method needs to be yielded from
+        # https://docs.python.org/3/reference/expressions.html#yieldexpr
+        # Probably better: handle this in the game loop, i.e. enter a glide mode 
+        # and only after the glide mode finished, the steps are continued.
+        pass
+
+    def glide_to_mouse_pointer(self, secs):
+        pass
+
+    def glide_to_sprite(self, sprite, secs):
+        pass
+
+    def glide_to_x_y(self, x, y, secs):
+        pass
+
+    def point_in_direction(self, direction):
+        pass
+
+    def point_towards_mouse_pointer(self):
+        pass
+
+    def point_towards_sprite(self, sprite):
+        pass
+
+    def change_x_by(self, value):
+        pass
+
+    def set_x_to(self, value):
+        pass
+
+    def change_y_by(self, value):
+        pass
+
+    def set_u_to(self, value):
+        pass
+
+    def if_on_edge_bounce(self):
+        pass
+
+    def set_rotation_style(self, style):
+        # See Enum RotationStyle above
+        pass
+
+
+    # Setters and getters are questionable, but 
+    # this way we would clearly adapt the Scratch API
+    # We could get rid of them for a direct access 
+    # to x, y, direction and so on. Direct access if of 
+    # course available anyway.
+    def get_x_position(self):
+        pass
+
+    def get_y_position(self):
+        pass
+
+    def get_direction(self):
+        pass
 
 
 class Stage():
