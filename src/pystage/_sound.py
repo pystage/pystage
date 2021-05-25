603477295a2bc12aa097b17c7a7159850433ad74
Florian Rupp <florian.rupp95@gmail.com>
2021-05-25 14:51:20 +0200

    Implementation of basic sound operations.

--- src/pystage/_sound.py
+++ src/pystage/_sound.py
@@ -1,19 +1,28 @@
-class _Sound():
+from pygame.mixer import music
+import pygame
+
+
+class _Sound:
     ##
     # Sound
     #
 
     # Like for costumes and backdrops, we need a class structure here.
     # Plus a global sound manager.
+    def __init__(self):
+        self.mixer = pygame.mixer
+        self.mixer.init()
+
+    def start_sound(self, sound, loop=-1):
+        channel = self.mixer.find_channel()
+        channel.play(self.mixer.Sound(sound), loop)
+        return channel
 
     def play_sound_until_done(self, sound):
-        pass
-
-    def start_sound(self, sound):
-        pass
+        return self.start_sound(sound, 0)
 
     def stop_all_sounds(self):
-        pass
+        self.mixer.stop()
 
     def change_sound_effect_by(self, effect, value):
         # Similar to graphics effects
@@ -29,13 +38,15 @@ class _Sound():
     def clear_sound_effects(self):
         pass
 
-    def change_volume_by(self, value):
-        # percent value
-        pass
+    @staticmethod
+    def change_volume_by(value):
+        actual_volume = music.get_volume()
+        music.set_volume(actual_volume + value)
 
-    def set_volume_to_percent(self, percent):
-        pass
+    @staticmethod
+    def set_volume_to_percent(percent):
+        music.set_volume(percent)
 
-    def get_volume(self):
-        pass
-
+    @staticmethod
+    def get_volume():
+        return music.get_volume()
