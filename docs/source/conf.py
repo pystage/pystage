6d158dfa942e52e4cd51440cfe6ec5c57055f816
Florian Rupp <florian.rupp95@gmail.com>
2021-05-28 15:10:45 +0200

    Including Readme file in docu. Minor docu fixes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,5 @@
 import sphinx_rtd_theme
-
+import myst_parser
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -33,7 +33,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', "sphinx_rtd_theme"]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', "sphinx_rtd_theme", "m2r2"]
 # 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,13 +3,15 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to PyStage Documentation's documentation!
+Welcome to PyStage's documentation!
 =================================================
 
 .. toctree::
+   :caption: Documentation:
    :maxdepth: 2
-   :caption: Contents:
 
+
+   readme_link
    modules
 
 
@@ -19,4 +21,3 @@ Indices and tables
 
 * :ref:`genindex`
 * :ref:`modindex`
-* :ref:`search`
